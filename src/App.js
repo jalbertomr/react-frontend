3e3177c3e761d81cfcfed5597ebe6ae16875493a
jalbertomr <jalbertomr@yahoo.com>
2021-06-07 18:29:45 -0500

    added ListPersonComponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import ListPersonComponent from "./components/ListPersonComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React Front-End!...</h1>
-      </header>
+    <div className="container">
+      <ListPersonComponent />
     </div>
   );
 }
---
+++ src/components/ListPersonComponent.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+class ListPersonComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { persons: [] };
+  }
+
+  render() {
+    return (
+      <div>
+        <h2 className="text-center">Persons List</h2>
+        <div className="row">
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>Person First Name</th>
+                <th>Person Last Name</th>
+                <th>Person email</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.persons.map((person) => (
+                <tr key={person.id}>
+                  <td> {person.firstName}</td>
+                  <td> {person.lastName}</td>
+                  <td> {person.email}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ListPersonComponent;
