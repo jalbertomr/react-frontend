b7cb0bdaa7566fe1c7d24e4cc76b60b7fa9934af
jalbertomr <jalbertomr@yahoo.com>
2021-06-07 20:01:38 -0500

    added route for /, /persons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ListPersonComponent from "./components/ListPersonComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
@@ -7,12 +8,18 @@ import FooterComponent from "./components/FooterComponent";
 function App() {
   return (
     <div>
-      <HeaderComponent />
-      <br />
-      <div className="container">
-        <ListPersonComponent />
-      </div>
-      <FooterComponent />
+      <Router>
+        <HeaderComponent />
+        <br />
+        <div className="container">
+          <Switch>
+            <Route path="/" component={ListPersonComponent}></Route>
+            <Route path="/persons" component={ListPersonComponent}></Route>
+            <ListPersonComponent />
+          </Switch>
+        </div>
+        <FooterComponent />
+      </Router>
     </div>
   );
 }
